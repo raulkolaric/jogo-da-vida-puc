d049a6fa273c31d63d608b10afaf8afe36f05bf8
Raul Kolaric <raul.kolaric@gmail.com>
2025-10-29 17:49:27 -0300

    struct and global variables for e4

--- JVIDA-KLRR-MODEL.h
+++ JVIDA-KLRR-MODEL.h
@@ -5,6 +5,12 @@
 //Raul Kolaric
 //Rodrigo Ward Leite
 
+typedef struct cel {
+	int lin;
+	int col;
+	struct cel *next;
+} TipoCel;
+
 //Variaveis globais
 int coordenadaX, coordenadaY;		//Coordenadas da célula a ser manipulada
 int dimensao;						//Dimensão do tabuleiro (tamanho da matriz)
@@ -13,3 +19,14 @@ int geracao = 1;					//Número da geração atual do jogo
 char matriz[60][60];				//Matriz principal representando o estado atual do jogo
 char matrizAuxiliar[60][60];		//Matriz auxiliar usada para calcular a próxima geração
 int mostrar = 0;					//Flag para mostrar (1) ou esconder (0) vizinhos mortos
+
+//Ponteiros para o inicio das listas
+TipoCel *pvivo = NULL;
+TipoCel *pmorto = NULL;
+TipoCel *pvivoprox = NULL;
+
+//Contadores de celulas em cada lista
+int totvivo = 0;
+int totmorto = 0;
+int totvivoprox = 0;
+
